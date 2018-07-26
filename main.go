73aeab214a32a3fe884e77f425f67dce4e63e053
fwchen <crystal.tyan@gmail.com>
2018-07-26 10:55:32 +0800

    update

--- main.go
+++ main.go
@@ -121,7 +121,7 @@ func main() {
 	fmt.Println("jellyfish serve on http://0.0.0.0:8020")
 
 	c := cron.New()
-	c.AddFunc("0 30 9 * * *", func() { // every day 1 am
+	c.AddFunc("0 11 * * * *", func() { // every day 1 am
 		schedulers.LogArticleHistory(db)
 	})
 	c.Start()

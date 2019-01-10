f36777f41014c044269b6d6df9f1a6370cd23997
fwchen <me@chenfangwei.com>
2019-01-10 10:01:27 +0800

    add gorm for connect pg

--- database/db.go
+++ database/db.go
@@ -2,6 +2,11 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/spf13/viper"
+
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -17,6 +22,22 @@ func InitDB(filepath string) *sql.DB {
 	// we exit as well
 	if db == nil {
 		panic("db nil")
+	}
+	return db
+}
+
+func ConnectDatabase() *gorm.DB {
+	dbHost := viper.GetString("DB_HOST")
+	dbPort := viper.GetString("DB_PORT")
+	dbUser := viper.GetString("DB_USER")
+	dbName := viper.GetString("DB_NAME")
+	dbPasswd := viper.GetString("DB_PASSWD")
+
+	connectConfig := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", dbHost, dbPort, dbUser, dbName, dbPasswd)
+
+	db, err := gorm.Open("postgres", connectConfig)
+	if err != nil {
+		panic(err)
 	}
 	return db
 }

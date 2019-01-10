490c514d32418d60c5b1c52466c2204c1c6ba50e
fwchen <me@chenfangwei.com>
2019-01-10 10:01:40 +0800

    add entitys and domains

---
+++ entitys/article.go
@@ -0,0 +1,16 @@
+package entitys
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Article struct {
+	gorm.Model
+	Name         string
+	Email        string  `gorm:"type:varchar(100);unique_index"`
+	Role         string  `gorm:"size:255"`        // set field size to 255
+	MemberNumber *string `gorm:"unique;not null"` // set member number to unique and not null
+	Num          int     `gorm:"AUTO_INCREMENT"`  // set num to auto incrementable
+	Address      string  `gorm:"index:addr"`      // create index with name `addr` for address
+	IgnoreMe     int     `gorm:"-"`               // ignore this field
+}
